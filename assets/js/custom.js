8040ea45dd1f78e5c4c1c30858f0ed57cc7796f2
BINIL E B <binilbalan.e.k@gmail.com>
2023-05-07 20:55:51 +0530

    index page complated

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,6 +5,8 @@ const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstra
 //copy text
 const copyEmail = document.getElementById("copyEmail")
 const copyPhone = document.getElementById("copyPhone")
+const copyEmail2 = document.getElementById("copyEmail2")
+const copyPhone2 = document.getElementById("copyPhone2")
 
 copyEmail.addEventListener('click',()=>{
     const emailCopy = document.getElementById("emailCopy")
@@ -16,4 +18,27 @@ copyPhone.addEventListener('click',()=>{
     const phoneCopy = document.getElementById("phoneCopy")
     navigator.clipboard.writeText(phoneCopy.innerText)
     console.log(phoneCopy.innerText);
+})
+
+copyEmail2.addEventListener('click',()=>{
+    const emailCopy = document.getElementById("emailCopy")
+    navigator.clipboard.writeText(emailCopy.innerText)
+    console.log(emailCopy.innerText,'1');  
+})
+
+copyPhone2.addEventListener('click',()=>{
+    const phoneCopy = document.getElementById("phoneCopy")
+    navigator.clipboard.writeText(phoneCopy.innerText)
+    console.log(phoneCopy.innerText);
+})
+
+document.getElementById("sendMail").addEventListener('click',()=>{
+    let name = document.getElementById("name").value
+    // let email = document.getElementById("email").value
+    let subject = document.getElementById("subject").value
+    let msg = document.getElementById("message").value
+    let message = msg.charAt(0).toUpperCase()+msg.slice(1)
+    let body = `Hai Binil Iam ${name}, ${message}`   
+    
+    window.location.href = "mailto:" + email + "?subject=" + subject + "&body=" + body;
 })
